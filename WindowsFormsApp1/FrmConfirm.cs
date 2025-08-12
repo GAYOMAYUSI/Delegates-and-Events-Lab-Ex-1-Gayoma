0ce880bac81806112ee1f2e8e180ee6dcc3dd3d1
aujsc <aujsc@GAYOMAYUSI>
2025-08-12 19:35:44 +0800

    Add project files.

---
+++ WindowsFormsApp1/FrmConfirm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WindowsFormsApp1.StudentInfoClass;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmConfirm : Form
+    {
+
+        private DelegateText DelProgram, DelLastName, DelFirstName, DelMiddleName, DelAddress;
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private DelegateNumber DelNumAge, DelNumContactNo, DelStudNo;
+
+        public FrmConfirm()
+        {
+            InitializeComponent();
+
+            DelProgram = new DelegateText(GetProgram);
+            DelLastName = new DelegateText(GetLastName);
+            DelFirstName = new DelegateText(GetFirstName);
+            DelMiddleName = new DelegateText(GetMiddleName);
+            DelAddress = new DelegateText(GetAddress);
+            DelNumAge = new DelegateNumber(GetAge);
+            DelNumContactNo = new DelegateNumber(GetContactNo);
+            DelStudNo = new DelegateNumber(GetStudentNo);
+
+            labelFirstName.Text = DelFirstName(FirstName);
+            labelLastName.Text = DelLastName(LastName);
+            labelMiddleName.Text = DelMiddleName(MiddleName);
+            labelAddress.Text = DelAddress(Address);
+            labelProgram.Text = DelProgram(StudentInfoClass.Program);
+            labelAge.Text = DelNumAge(Age).ToString();
+            labelContactNo.Text = DelNumContactNo(ContactNo).ToString();
+            labelStudentNo.Text = DelStudNo(StudentNo).ToString();
+        }
+    }
+}
